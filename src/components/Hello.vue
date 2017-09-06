ee559ba6d6ac24d8a0f09234319b9b04697f2e35
Lisbon <act.imac.lisbon@icloud.com>
2017-09-06 15:31:17 +0900

    Initial commit

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -3,31 +3,31 @@
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li>
+        <div class="button" @click="call">call to server</div>
+      </li>
+      <li>{{testData}}</li>
     </ul>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axois from 'axios'
+
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        testData: 'testData'
+      }
+    },
+    methods: {
+      call: function () {
+        this.$data.testData = 'call to server button clicked!'
+        axois.get(process.env.API_URL + '/test')
+      }
     }
-  }
 }
 </script>
 
@@ -50,4 +50,12 @@ li {
 a {
   color: #42b983;
 }
+  .button {
+    height: 24px;
+    background-color: antiquewhite;
+    cursor: pointer;
+    border: 1px solid lightgray;
+    font-size: 16px;
+    padding: 10px;
+  }
 </style>
